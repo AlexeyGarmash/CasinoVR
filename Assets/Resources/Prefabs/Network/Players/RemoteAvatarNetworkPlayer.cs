fd3618d4b816970794bf6d467dd11cdbdf11eccc
AlexeyGarmash <i.republic2014@gmail.com>
2020-07-21 15:38:59 +0300

    changed remote avatar prefab

---
+++ Assets/Resources/Prefabs/Network/Players/RemoteAvatarNetworkPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteAvatarNetworkPlayer : MonoBehaviourPun
+{
+    [SerializeField] private string _ovrControllerName;
+
+    void Start()
+    {
+        if (photonView != null && photonView.IsMine)
+        {
+            GameObject globalVRController = GameObject.Find(_ovrControllerName);
+            if (globalVRController != null)
+            {
+                Transform ovrControllerTransform = globalVRController.transform;
+                Transform CameraRig = ovrControllerTransform.Find("OVRCameraRig").transform;
+                transform.SetParent(CameraRig, false);
+            }
+        }
+    }
+}
