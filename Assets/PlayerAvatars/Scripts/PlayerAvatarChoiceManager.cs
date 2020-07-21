72413331041690e1df6aaa0916fdd41868dc5bdf
AlexeyGarmash <i.republic2014@gmail.com>
2020-07-21 15:12:27 +0300

    merge with Sergey

---
+++ Assets/PlayerAvatars/Scripts/PlayerAvatarChoiceManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAvatarChoiceManager : MonoBehaviour
+{
+    [SerializeField] private PlayerAvatar[] _playerAvatars;
+    [SerializeField] private Transform _avatarPreviewSpawnPoint;
+
+    private void Start()
+    {
+        foreach (var avatar in _playerAvatars)
+        {
+            avatar.OnAvatarChosen += OnAvatarChosen;
+        }
+    }
+
+    private void OnAvatarChosen(PlayerAvatar avatar)
+    {
+        ChooseUiAvatar(avatar);
+        ClearNotSelectedSelection(avatar);
+        SpawnPreviewAvatar(avatar);
+    }
+
+    private void ChooseUiAvatar(PlayerAvatar avatar)
+    {
+        avatar.ChooseAvatar();
+    }
+
+    private void ClearNotSelectedSelection(PlayerAvatar avatar)
+    {
+        foreach (var _avatar in _playerAvatars)
+        {
+            if (avatar != _avatar)
+            {
+                _avatar.UncheckSelected();
+            }
+        }
+    }
+
+    private void SpawnPreviewAvatar(PlayerAvatar avatar)
+    {
+        ClearSpawnChildren();
+        SpawnPreview(avatar);
+    }
+
+    private void SpawnPreview(PlayerAvatar avatar)
+    {
+        if (_avatarPreviewSpawnPoint != null)
+        {
+            Instantiate(avatar.AvatarPrefab, _avatarPreviewSpawnPoint, false);
+        }
+    }
+
+    private void ClearSpawnChildren()
+    {
+        if(_avatarPreviewSpawnPoint != null)
+        {
+            for (int i = 0; i < _avatarPreviewSpawnPoint.childCount; i++)
+            {
+                Destroy(_avatarPreviewSpawnPoint.GetChild(i).gameObject);
+            }
+        }
+    }
+}
