9664e4b18e59596720743ef343f00cd75e7e4732
AlexeyGarmash <i.republic2014@gmail.com>
2020-08-07 13:46:57 +0300

    npc animator

---
+++ Assets/NPC/NpcRoulette.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcRoulette : MonoBehaviour
+{
+    private const string SPIN_ANIM = "Spin";
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RunSpinAnimation()
+    {
+        
+    }
+
+    
+
+}
