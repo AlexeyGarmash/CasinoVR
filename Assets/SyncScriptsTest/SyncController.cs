18628cd2fdcbf74039482e96cb18180ed2a8505d
AlexeyGarmash <i.republic2014@gmail.com>
2020-08-11 10:55:56 +0300

    fix too much RPCs

--- Assets/SyncScriptsTest/SyncController.cs
+++ Assets/SyncScriptsTest/SyncController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SyncController : MonoBehaviourPun
 {
     private OVRGrabbableCustom _customGrabbable;
-
+    private bool _isGrabbed = false;
     void Awake()
     {
         _customGrabbable = GetComponent<OVRGrabbableCustom>();
@@ -16,22 +16,26 @@ public class SyncController : MonoBehaviourPun
     void Update()
     {
         if(_customGrabbable == null) return;
-        if(_customGrabbable.isGrabbed) {
+        if(_customGrabbable.isGrabbed && !_isGrabbed) {
             photonView.RPC("SyncOn_RPC", RpcTarget.AllBuffered);
+            print("Sync -> on");
         }
-        else {
+        else if(!_customGrabbable.isGrabbed && _isGrabbed) {
             photonView.RPC("SyncOff_RPC", RpcTarget.AllBuffered);
+            print("Sync -> off");
         }
     }
 
 
     [PunRPC]
     public void SyncOn_RPC() {
+        _isGrabbed = true;
         photonView.Synchronization = ViewSynchronization.UnreliableOnChange;
     }
 
     [PunRPC]
     public void SyncOff_RPC() {
+        _isGrabbed = false;
         photonView.Synchronization = ViewSynchronization.Off;
     }
 }
