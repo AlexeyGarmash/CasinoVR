1a50c164d32b2fbb8cc38513d9a77d665a8b42a2
Alex <i.republic2014@gmail.com>
2020-09-07 16:16:32 +0300

    playable piano

---
+++ Assets/SomePiano/PianoButton.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoButton : MonoBehaviour
+{
+    [SerializeField] private float pressLength;
+    public int index;
+    private PianoButtonSound buttonSound;
+    public bool pressed;
+    private Vector3 localStartPos;
+    private bool clickPerformed;
+    public PianoThumb currentPianoThumb;
+    public float observDistance;
+
+    private bool thumbExit = false;
+
+    private void Start()
+    {
+        localStartPos = transform.localPosition;
+        index = transform.GetSiblingIndex();
+        buttonSound = GetComponent<PianoButtonSound>();
+    }
+
+    private void Update()
+    {
+        if(thumbExit)
+        {
+            float distance = Vector3.Distance(currentPianoThumb.transform.position, transform.position);
+            if (distance > 0.08)
+            {
+                pressed = false;
+                currentPianoThumb = null;
+                thumbExit = false;
+            }
+        }
+        if(pressed)
+        {
+            if (!clickPerformed)
+            {
+                transform.localPosition = new Vector3(localStartPos.x, localStartPos.y, localStartPos.z + pressLength);
+                //some action
+                MakeKeyActionDown();
+                print(string.Format("Button {0} clicked", gameObject.name));
+                clickPerformed = true;
+            }
+        }
+        else
+        {
+            if (clickPerformed)
+            {
+                transform.localPosition = new Vector3(localStartPos.x, localStartPos.y, localStartPos.z);
+                MakeKeyActionUp();
+                clickPerformed = false;
+            }
+        }
+    }
+
+    private void MakeKeyActionDown()
+    {
+        buttonSound.PlayKeySound(index);
+    }
+
+    private void MakeKeyActionUp()
+    {
+        buttonSound.StopKeySound(-1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var pianoThumb = other.GetComponent<PianoThumb>();
+        if(pianoThumb != null && !pressed)
+        {
+            observDistance = Vector3.Distance(pianoThumb.transform.position, transform.position);
+            thumbExit = false;
+            pressed = true;
+            currentPianoThumb = pianoThumb;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var pianoThumb = other.GetComponent<PianoThumb>();
+        if(pianoThumb != null && pressed && pianoThumb == currentPianoThumb)
+        {
+            /*float distance = Vector3.Distance(pianoThumb.transform.position, transform.position);
+            if (distance > 0.07)
+            {
+                pressed = false;
+                currentPianoThumb = null;
+            }*/
+            thumbExit = true;
+        }
+    }
+
+
+
+}
---
+++ Assets/SomePiano/PianoSounds.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PianoSounds : MonoBehaviour
+{
+    public bool isMainKeys = true;
+    public AudioClip[] clips;
+    public List<string> mainKeys = new List<string>();
+    public List<string> secondaryKeys = new List<string>();
+
+    private void Start()
+    {
+        LoadFromResources();
+        mainKeys.Add("__a0");
+        mainKeys.Add("__b0");
+        secondaryKeys.Add("__a-0");
+        GenerateKeys();
+    }
+
+    private string[] lettersMain = { "a", "b", "c", "d", "e", "f", "g" };
+    private string[] lettersSecond = { "a", "c", "d", "f", "g" };
+    private void GenerateKeys()
+    {
+        if (isMainKeys)
+        {
+            int k = 0;
+            int ind = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                if(k >= 7)
+                {
+                    k = 0;
+                    ind++;
+                }
+                mainKeys.Add("__" + lettersMain[k] + (ind).ToString());
+                k++;
+            }
+        }
+        else
+        {
+            int k = 0;
+            int ind = 1;
+            for (int i = 0; i < 35; i++)
+            {
+                if (k >= 5)
+                {
+                    k = 0;
+                    ind++;
+                }
+                secondaryKeys.Add("__" + lettersSecond[k] + "-" + (ind).ToString());
+                k++;
+            }
+        }
+    }
+
+    private void LoadFromResources()
+    {
+        clips = Resources.LoadAll<AudioClip>("Audio/piano_sounds");
+    }
+
+    public AudioClip GetKeySound(int index)
+    {
+        string strIndex = isMainKeys ? mainKeys[index] : secondaryKeys[index];
+        AudioClip findClip = clips.FirstOrDefault(cl => cl.name.Contains(strIndex));
+        return findClip;
+    }
+
+}
