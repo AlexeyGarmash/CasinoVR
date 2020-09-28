c246062cb44c6120474c2d8521ee10937c90631b
Alex <i.republic2014@gmail.com>
2020-09-28 15:48:48 +0300

    add guides for games

---
+++ Assets/GuideMenus/GuideLookAtPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideLookAtPlayer : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(2 * transform.position - Camera.main.transform.position);
+    }
+}
