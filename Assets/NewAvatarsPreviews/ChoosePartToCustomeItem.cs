ac925bea0ad5be5eb5a46bf9d4ce50ab4a77a85b
Alex <i.republic2014@gmail.com>
2020-10-13 17:57:21 +0300

    add avatars props to customize

---
+++ Assets/NewAvatarsPreviews/ChoosePartToCustomeItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[Serializable]
+public class BodyPart
+{
+    public CustomizeAvatarManager.CustomizePart PartToCustomize;
+    public string PartNameToCustomize;
+}
+
+public class ChoosePartToCustomeItem : MonoBehaviour
+{
+    public Action<BodyPart> OnCustomizePartChanged;
+    public BodyPart BodyPart;
+    private Button btnChooseItem;
+
+    private void Start()
+    {
+        btnChooseItem = GetComponent<Button>();
+        btnChooseItem.onClick.AddListener(OnBtn_Clicked);
+    }
+
+    private void OnBtn_Clicked()
+    {
+        if (OnCustomizePartChanged != null)
+        {
+            OnCustomizePartChanged.Invoke(BodyPart);
+        }
+    }
+}
---
+++ Assets/ToDelete/CheckOculusUser.cs
@@ -0,0 +1,36 @@
+﻿using Oculus.Platform;
+using Oculus.Platform.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOculusUser : MonoBehaviour
+{
+    private void Start()
+    {
+        Oculus.Platform.Core.Initialize("3300730370034575");
+        var userRequest = Oculus.Platform.Users.GetLoggedInUser();
+        if(userRequest != null)
+        {
+            userRequest.OnComplete(GetLoggedInUserCallback);
+        }
+        else
+        {
+            print("USER GET FAILED");
+        }
+    }
+
+    private void GetLoggedInUserCallback(Message<User> message)
+    {
+        if (!message.IsError)
+        {
+            print($"[OCULUS CHECKER] Oculus user = {message.GetUser().ID}");
+            print($"[OCULUS CHECKER] Oculus user = {message.GetUser().OculusID}");
+        }
+        else
+        {
+            print($"[OCULUS CHECKER] Error while get user = {message.GetError().Message}");
+        }
+    }
+}
